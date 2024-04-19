8296ab10a2b5131a7567ac8886a6ecfe6ef48f5f
Priyanshusingh3192 <priyanshusingh202010@gmail.com>
2024-04-20 03:02:49 +0530

    MMMM
    MMMMM

--- server/model/User.js
+++ server/model/User.js
@@ -16,7 +16,14 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
     pwd: {
         type: String,
         required: true
