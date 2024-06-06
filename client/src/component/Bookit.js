5a50f0f264d0928c1d9ebee334a13b5167af6061
Priyanshusingh3192 <priyanshusingh202010@gmail.com>
2024-06-07 03:22:41 +0530

    payment integration
    payment integration

--- client/src/component/Bookit.js
+++ client/src/component/Bookit.js
@@ -6,11 +6,10 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { Box, CircularProgress, LinearProgress } from "@mui/material";
 
-
 const Bookit = () => {
   const axios = useAxiosPrivate();
   const { arrivalDate, departureDate, newUser } = useContext(User);
-  const { id, company } = useParams(); // Get the company ID from URL params
+  const { id, company } = useParams();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     compid: id,
@@ -30,16 +29,14 @@ const Bookit = () => {
       [e.target.name]: e.target.value
     });
   };
-  const [loading,setLoading]=useState(false)
-
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true)
+    setLoading(true);
 
     try {
-      
-      await toast(" Successful! Redirecting to the  page!");
+      toast("Successful! Redirecting to the page!");
 
       const response = await axios.post('/park/bookit', formData);
       if (response.status !== 200) {
@@ -47,7 +44,6 @@ const Bookit = () => {
       }
       console.log('Booking successful:', response.data);
 
-      // Navigate to the ticket page with form data and booking key
       navigate('/ticket', {
         state: {
           formData,
@@ -55,9 +51,8 @@ const Bookit = () => {
           bookingKey: response.data.key
         }
       });
-      setLoading(false)
+      setLoading(false);
 
-      // Reset form fields (optional)
       setFormData({
         compid: id,
         compName: company,
@@ -70,180 +65,176 @@ const Bookit = () => {
       });
     } catch (error) {
       console.error('Error booking the slot:', error.message);
+      setLoading(false);
     }
   };
 
+  const [amount, setAmount] = useState(350);
 
-  const [amount, setamount] = useState(350);
-
-  // handlePayment Function
   const handlePayment = async () => {
-      try {
-          const res = await fetch("http://localhost:3500/api/payment/order", {
-              method: "POST",
-              headers: {
-                  "content-type": "application/json"
-              },
-              body: JSON.stringify({
-                  amount
-              })
+    try {
+      const res = await fetch("http://localhost:3500/api/payment/order", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ amount })
+      });
+
+      const data = await res.json();
+      console.log(data);
+      handlePaymentVerify(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlePaymentVerify = async (data) => {
+    const options = {
+      key: "rzp_test_pVw2xUhCDTWUUw",
+      amount: data.amount,
+      currency: data.currency,
+      name: "SafeNet",
+      description: "Test Mode",
+      order_id: data.id,
+      handler: async (response) => {
+        console.log("response", response);
+        try {
+          const res = await fetch("http://localhost:3500/api/payment/verify", {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+              razorpay_order_id: response.razorpay_order_id,
+              razorpay_payment_id: response.razorpay_payment_id,
+              razorpay_signature: response.razorpay_signature,
+            })
           });
 
-          const data = await res.json();
-          console.log(data);
-          handlePaymentVerify(data.data)
-      } catch (error) {
-          console.log(error);
-      }
-  }
-
-  // handlePaymentVerify Function
-  const handlePaymentVerify = async (data) => {
-      const options = {
-          // key: import.meta.env.RAZORPAY_KEY_ID,
-          key:"rzp_test_pVw2xUhCDTWUUw",
-          amount: data.amount,
-          currency: data.currency,
-          name: "SafeNet",
-          description: "Test Mode",
-          order_id: data.id,
-          handler: async (response) => {
-              console.log("response", response)
-              try {
-                  const res = await fetch("http://localhost:3500/api/payment/verify", {
-                      method: 'POST',
-                      headers: {
-                          'content-type': 'application/json'
-                      },
-                      body: JSON.stringify({
-                          razorpay_order_id: response.razorpay_order_id,
-                          razorpay_payment_id: response.razorpay_payment_id,
-                          razorpay_signature: response.razorpay_signature,
-                      })
-                  })
-
-                  const verifyData = await res.json();
-
-                  if (verifyData.message) {
-                      toast.success(verifyData.message)
-                  }
-              } catch (error) {
-                  console.log(error);
-              }
-          },
-          theme: {
-              color: "#5f63b8"
+          const verifyData = await res.json();
+          if (verifyData.message) {
+            toast.success(verifyData.message);
           }
-      };
-      const rzp1 = new window.Razorpay(options);
-      rzp1.open();
-  }
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      theme: {
+        color: "#5f63b8"
+      }
+    };
+
+    const rzp1 = new window.Razorpay(options);
+    rzp1.open();
+  };
 
   return (
     <div style={styles.container}>
       <h2>Book Parking Slot</h2>
       <form onSubmit={handleSubmit} style={styles.form}>
-      <div style={styles.inputGroup}>
-  <label htmlFor="licensePlate" style={styles.label}>License Plate:</label>
-  <input
-    type="text"
-    id="licensePlate"
-    name="licensePlate"
-    value={formData.licensePlate}
-    onChange={handleChange}
-    required
-    style={styles.input}
-  />
-</div>
-<div style={styles.inputGroup}>
-  <label htmlFor="vehicleType" style={styles.label}>Vehicle Type:</label>
-  <input
-    type="text"
-    id="vehicleType"
-    name="vehicleType"
-    value={formData.vehicleType}
-    onChange={handleChange}
-    required
-    style={styles.input}
-  />
-</div>
-<div style={styles.inputGroup}>
-  <label htmlFor="ownerName" style={styles.label}>Owner Name:</label>
-  <input
-    type="text"
-    id="ownerName"
-    name="ownerName"
-    value={formData.ownerName}
-    onChange={handleChange}
-    required
-    style={styles.input}
-  />
-</div>
-<div style={styles.inputGroup}>
-  <label htmlFor="phone" style={styles.label}>Phone:</label>
-  <input
-    type="text"
-    id="phone"
-    name="phone"
-    value={formData.phone}
-    onChange={handleChange}
-    required
-    style={styles.input}
-  />
-</div>
-<div style={styles.inputGroup}>
-  <label htmlFor="startTime" style={styles.label}>Start Time:</label>
-  <input
-    type="datetime-local"
-    id="startTime"
-    name="startTime"
-    value={formData.startTime}
-    onChange={handleChange}
-    style={styles.input}
-  />
-</div>
-<div style={styles.inputGroup}>
-  <label htmlFor="endTime" style={styles.label}>End Time:</label>
-  <input
-    type="datetime-local"
-    id="endTime"
-    name="endTime"
-    value={formData.endTime}
-    onChange={handleChange}
-    style={styles.input}
-  />
-</div>
+        <div style={styles.inputGroup}>
+          <label htmlFor="licensePlate" style={styles.label}>License Plate:</label>
+          <input
+            type="text"
+            id="licensePlate"
+            name="licensePlate"
+            value={formData.licensePlate}
+            onChange={handleChange}
+            required
+            style={styles.input}
+          />
+        </div>
+        <div style={styles.inputGroup}>
+          <label htmlFor="vehicleType" style={styles.label}>Vehicle Type:</label>
+          <input
+            type="text"
+            id="vehicleType"
+            name="vehicleType"
+            value={formData.vehicleType}
+            onChange={handleChange}
+            required
+            style={styles.input}
+          />
+        </div>
+        <div style={styles.inputGroup}>
+          <label htmlFor="ownerName" style={styles.label}>Owner Name:</label>
+          <input
+            type="text"
+            id="ownerName"
+            name="ownerName"
+            value={formData.ownerName}
+            onChange={handleChange}
+            required
+            style={styles.input}
+          />
+        </div>
+        <div style={styles.inputGroup}>
+          <label htmlFor="phone" style={styles.label}>Phone:</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+            style={styles.input}
+          />
+        </div>
+        <div style={styles.inputGroup}>
+          <label htmlFor="startTime" style={styles.label}>Start Time:</label>
+          <input
+            type="datetime-local"
+            id="startTime"
+            name="startTime"
+            value={formData.startTime}
+            onChange={handleChange}
+            style={styles.input}
+          />
+        </div>
+        <div style={styles.inputGroup}>
+          <label htmlFor="endTime" style={styles.label}>End Time:</label>
+          <input
+            type="datetime-local"
+            id="endTime"
+            name="endTime"
+            value={formData.endTime}
+            onChange={handleChange}
+            style={styles.input}
+          />
+        </div>
 
-<button
-      onClick={handlePayment}
-      className="mb-5 w-full bg-[#1B9CFC] text-white py-2 px-4 rounded-md hover:bg-[#1781cc] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#1B9CFC]"
-    >
-      Pay ₹ 10
-    </button>
+        <button
+          type="button"
+          onClick={handlePayment}
+          className="mb-5 w-full bg-[#1B9CFC] text-white py-2 px-4 rounded-md hover:bg-[#1781cc] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#1B9CFC]"
+        >
+          Pay ₹ 10
+        </button>
 
-{loading ? (
-               <button className="rounded">
-                <Box sx={{ width: '100%', height: '20px' }}>
-                  <LinearProgress color="secondary" sx={{ height: '5vh' }} />
-                </Box>
-              </button>
-            ):(
-              <button type="submit" style={styles.button}>Book Slot</button>
-            )}
-
+        {loading ? (
+          <Box sx={{ width: '100%', height: '20px' }}>
+            <LinearProgress color="secondary" sx={{ height: '5vh' }} />
+          </Box>
+        ) : (
+          <button type="submit" style={styles.button}>Book Slot</button>
+        )}
       </form>
+      <ToastContainer />
     </div>
   );
 };
+
 export default Bookit;
+
 const styles = {
   container: {
-    margin: '20px auto', // Center horizontally
+    margin: '20px auto',
     padding: '20px',
     border: '1px solid #ccc',
     borderRadius: '10px',
     backgroundColor: '#f9f9f9',
-    maxWidth: '400px', // Reduced width
+    maxWidth: '400px',
   },
   form: {
     display: 'flex',
@@ -253,14 +244,14 @@ const styles = {
     marginBottom: '20px',
   },
   label: {
-    marginBottom: '5px', // Add margin to the bottom of the label
+    marginBottom: '5px',
   },
   input: {
     padding: '10px',
     border: '1px solid #ccc',
     borderRadius: '5px',
     fontSize: '16px',
-    width: '100%', // Make input fields occupy full width
+    width: '100%',
   },
   button: {
     padding: '10px 20px',
